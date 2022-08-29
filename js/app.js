25b7d7b387c4c02b2f70714d157d29c3cc32ad89
Maxi Daniele <maxidanielemg@gmail.com>
2022-08-29 20:20:25 -0300

    Se agrega async await a la api

--- js/app.js
+++ js/app.js
@@ -47,18 +47,27 @@ function mostrarAlerta(mensaje){
     }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
     const key = '28842317-f505c2c6ec8c46ecb0dbc9493';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
+    // fetch(url)
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits)
+    //         mostrarImagenes(resultado.hits);
+    //     });
+    
+        try {
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
             totalPaginas = calcularPaginas(resultado.totalHits)
             mostrarImagenes(resultado.hits);
-        })
+        } catch (error) {
+            console.log(error);
+        }
 }
 
 //Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
